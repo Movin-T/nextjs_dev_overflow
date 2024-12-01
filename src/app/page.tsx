c22e10271de76a1ffdc35d1c0475f471603ca766
Movin Tilakaratne <movin1999t@gmail.com>
2024-12-01 14:04:44 +0530

    ESLint & Prettier Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl text-violet-700 font-black">
-      Welcome to the world of Next.js
-    </h1>
-  );
-}
+const Home = () => (
+  <h1 className="text-3xl font-black text-white">
+    Welcome to the world of Next.js
+  </h1>
+);
+
+export default Home;

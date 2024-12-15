326f6b16df3cc6971a858e2982269843b205faf2
Movin Tilakaratne <movin1999t@gmail.com>
2024-12-15 08:51:38 +0530

    Implement Home Filters

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -13,10 +14,6 @@ const questions = [
       {
         _id: "1",
         name: "React",
-      },
-      {
-        _id: "2",
-        name: "JavaScript",
       },
     ],
     author: {
@@ -35,10 +32,6 @@ const questions = [
     tags: [
       {
         _id: "1",
-        name: "React",
-      },
-      {
-        _id: "2",
         name: "JavaScript",
       },
     ],
@@ -58,11 +51,19 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase())
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query?.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags.some(
+          (tag) => tag.name.toLowerCase() === filter.toLowerCase()
+        )
+      : true;
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -84,7 +85,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           otherClasses="flex-1"
         />
       </section>
-      {/* HomeFilter */}
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>

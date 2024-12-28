93be78c054d8261311f11a75d633e0abaaed6920
Movin Tilakaratne <movin1999t@gmail.com>
2024-12-28 15:38:12 +0530

    Create route handlers

---
+++ src/app/database.ts
@@ -0,0 +1,22 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next.js API Route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Add new feature to homepage",
+    status: "in progress",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Refactor TypeScript code",
+    status: "open",
+    type: "refactor",
+  },
+];
+
+export default tickets;

e73431c4ae86d318d5b3eb4829ca1853f3d7c7b6
Movin Tilakaratne <movin1999t@gmail.com>
2025-01-22 20:55:50 +0530

    Create empty state data renderer

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,6 +6,8 @@ import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import { getQuestions } from "@/lib/actions/question.action";
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_QUESTION } from "@/constants/states";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -56,7 +58,18 @@ const Home = async ({ searchParams }: SearchParams) => {
         />
       </section>
       <HomeFilter />
-      {success ? (
+      <DataRenderer
+        success={success}
+        error={error}
+        data={questions}
+        empty={EMPTY_QUESTION}
+        render={(questions) =>
+          questions.map((question) => (
+            <QuestionCard key={question._id} question={question} />
+          ))
+        }
+      />
+      {/* {success ? (
         <div className="mt-10 flex w-full flex-col gap-6">
           {questions && questions.length > 0 ? (
             questions.map((question) => (
@@ -74,7 +87,7 @@ const Home = async ({ searchParams }: SearchParams) => {
             {error?.message || "Failed to fetch questions"}
           </p>
         </div>
-      )}
+      )} */}
     </>
   );
 };
